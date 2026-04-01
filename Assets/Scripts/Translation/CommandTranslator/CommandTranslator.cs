f5193769861c2ac38757e2ce7d523648743994c9
georgeli1583 <georgeli@live.ca>
2026-03-31 22:26:32 -0700

    Save/save space pattern

--- Assets/Scripts/Translation/CommandTranslator/CommandTranslator.cs
+++ Assets/Scripts/Translation/CommandTranslator/CommandTranslator.cs
@@ -41,7 +41,7 @@ namespace T2G.Assistant
             (@"^(clear|clean)(?:\s+(history|chat|chat\s+history))?$", T2G.Actions.clear),
             (@"^create\s+space\s+(?<name>[^\s]+)$", T2G.Actions.create_space),
             (@"^(goto|enter|open)\s+space\s+(?<name>[^\s]+)$", T2G.Actions.goto_space),
-            (@"^save space$", T2G.Actions.save_space),
+            (@"^(save space|save)$", T2G.Actions.save_space),
             (@"^(?:\w+\s+)?(create|new|instantiate)\s+(?:a\s+|an\s+)?(?:new\s+)?(?<desc>[\w\s\-]+?)(?:\s+(?:called|named|name|with\s+the\s+name)\s+(?<name>""[^""]+""|[\w\s\-]+))?(?:\.)?$", T2G.Actions.create_object),
             (@"^(?:\w+\s+)?(select)(?:\s+(?:object))?\s+(?<name>""[^""]+""|'[^']+'|[\w\-\s]+?)(?:\.)?$", T2G.Actions.select_object),
             (@"^(?:\w+\s+)?(delete)(?:\s+(?:object))?\s+(?<name>""[^""]+""|'[^']+'|[\w\-\s]+?)(?:\.)?$", T2G.Actions.delete_object),
